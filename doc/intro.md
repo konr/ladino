8b216cdf4ab4c33688a017a7fb0f4ee9b5f8a9fb
Konrad Scorciapino <konr@konr.mobi>
2013-07-07 12:36:16 -0300

    first commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to ladino
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/ladino/core_test.clj
@@ -0,0 +1,7 @@
+(ns ladino.core-test
+  (:use clojure.test
+        ladino.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
