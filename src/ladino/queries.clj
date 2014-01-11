8705095c0a194528c354a71d74744e692d286a5e
Konrad Scorciapino <konr@konr.mobi>
2014-01-11 19:30:08 -0200

    removing trash from the queries repo

--- src/ladino/queries.clj
+++ src/ladino/queries.clj
@@ -7,33 +7,6 @@
             [datomic.api :as d]))
 
 
-
-
-(defn matches [attributes & [symbol symbol']]
-  (let [symbol (or symbol '?a)
-        symbol' (or symbol' '?b)]
-    (apply concat
-           (for [att attributes]
-             (let [new (symbol (str "?" "abc" #_(d/squuid)))]
-               [[symbol att new] [symbol' att new]])))))
-
-
-
-(defn find-matches [part {:keys [stem ending] :as obj}]
-  (case part
-    "ADJ" 42
-    "V" "Verbo"
-    "N" "Nuno"
-    #_(let [query {:find '[?a] :in '[$ ?stem ?ending]
-                   :where (concat (matches [:part-of-speech])
-                                  '[[?a :ending ?ending]
-                                    [?b :stem ?stem]])}]
-        (-<>> (lp/q query stem ending)
-
-
-              ))
-    ))
-
 (def rules
   '[[(same-part-of-speech ?a ?b ?pos)
      [?a :part-of-speech ?pos]
