6d9daff5a09841e03c0457928c00f429e5f09ae9
Konrad Scorciapino <konr@konr.mobi>
2014-01-14 00:12:14 -0200

    updated licensing on the README

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject ladino "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "a modern latin dictionary"
+  :url "http://github.com/konr/ladino"
+  :license {:name "GPL"
+            :url "http://www.gnu.org/licenses/"}
   :dependencies [[org.clojure/clojure "1.5.0"]
                  [prismatic/schema "0.1.8"]
                  [com.datomic/datomic-free "0.9.4384"]
@@ -11,6 +11,4 @@
   :plugins [[lein-midje "3.1.3"]]
   :profiles {:dev {:source-paths ["config"]
                    :dependencies [[midje "1.6.0" :exclusions [org.clojure/clojure]]]}}
-  :main ladino.core
-
-  )
+  :main ladino.core)
