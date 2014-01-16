b3e5fca18fd22806e67233ecc99d2ebefeab2633
Konrad Scorciapino <konr@konr.mobi>
2014-01-15 23:27:48 -0200

    source

--- src/ladino/sources/dictline.clj
+++ src/ladino/sources/dictline.clj
@@ -17,7 +17,7 @@
   nil)
 
 (defmethod second-parse "PRON" [{:keys [part-of-speech description words] :as map}]
-  (-> [:stem-1 :stem-2 :part-of-speech :declension :variant :kind "X" :area "X" "A" "O"]
+  (-> [:stem-1 :stem-2 :part-of-speech :declension :variant :kind "X" :area "X" "A" :source]
       #_(zipmap word)
       words
       ))
@@ -45,6 +45,34 @@
   "T" "Technical, Architecture, Topography, Surveying"
   "W" "War, Military, Naval, Ships, Armor"
   "Y" "Mythology"})
+
+(def source
+    "X"        "General or unknown or too common to say"
+    "A"        ""
+    "B"        "C.H.Beeson, A Primer of Medieval Latin, 1925 (Bee)"
+    "C"        "Charles Beard, Cassell's Latin Dictionary 1892 (Cas)"
+    "D"        "J.N.Adams, Latin Sexual Vocabulary, 1982 (Sex)"
+    "E"        "L.F.Stelten, Dictionary of Eccles. Latin, 1995 (Ecc)"
+    "F"        "Roy J. Deferrari, Dictionary of St. Thomas Aquinas, 1960 (DeF)"
+    "G"        "Gildersleeve + Lodge, Latin Grammar 1895 (G+L)"
+    "H"        "Collatinus Dictionary by Yves Ouvrard "
+    "I"        "Leverett, F.P., Lexicon of the Latin Language, Boston 1845"
+    "J"        "Bracton: De Legibus Et Consuetudinibus Angliæ"
+    "K"        "Calepinus Novus, modern Latin, by Guy Licoppe (Cal)"
+    "L"        "Lewis, C.S., Elementary Latin Dictionary 1891"
+    "M"        "Latham, Revised Medieval Word List, 1980 (Latham)"
+    "N"        "Lynn Nelson, Wordlist (Nel)"
+    "O"        "Oxford Latin Dictionary, 1982 (OLD)"
+    "P"        "Souter, A Glossary of Later Latin to 600 A.D., Oxford 1949 (Souter)"
+    "Q"        "Other, cited or unspecified dictionaries"
+    "R"        "Plater + White, A Grammar of the Vulgate, Oxford 1926 (Plater)"
+    "S"        "Lewis and Short, A Latin Dictionary, 1879 (L+S)"
+    "T"        "Found in a translation  --  no dictionary reference"
+    "U"        ""            
+    "V"        "Vademecum in opus Saxonis - Franz Blatt (Saxo)"
+    "W"        "My personal guess, mostly obvious extrapolation (Whitaker or W)   "
+    "Y"        "Temp special code"
+    "Z"        "Sent by user --  no dictionary reference")
 
 ;; function NUMBER_OF_STEMS(P : PART_OF_SPEECH_TYPE) return STEM_KEY_TYPE is
 ;; begin
