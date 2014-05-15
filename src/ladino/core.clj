e5d93d394d61d9793d0b33e719f662344a2c5992
Konrad Scorciapino <konr@konr.mobi>
2014-05-15 00:05:28 -0300

    Setting the tone

--- src/ladino/core.clj
+++ src/ladino/core.clj
@@ -19,26 +19,22 @@
 
 
 
-(defn read-sources []
-  (doseq [ending (lse/read-endings-file)]
-    (lp/transact-one ending))
-  (doseq [stem (lss/read-stemlist-file)]
-    (lp/transact-one stem)))
+(defn import-from-sources! []
+  (doseq [ending (lse/read-endings-file)] (lp/transact-one ending))
+  (doseq [stem (lss/read-stemlist-file)]  (lp/transact-one stem)))
 
 
-(defn init-db []
+(defn init-db! []
   (lp/init-db! {:uri (lp/random-uri)
              :extensions [(map #(assoc % :db/id (lp/tempid :db.part/db)) ext/schema-attributes)]
              :seed (map (partial map #(assoc % :db/id (lp/tempid :db.part/db))) enums/all-enums)
              :schemata [(lp/gen-attribute-seq lm/all-attributes)]}))
 
-(defn initialize []
-  (init-db)
-  (read-sources))
+(defn initialize! []
+  (init-db!)
+  (import-from-sources!))
 
 
 (defn -main [word]
-  (println "Initializing...")
-  (time (initialize))
-  (println (format "Parsing '%s'..." word))
-  (time (parse/parse word)))
+  (initialize)
+  (parse/parse word))
--- test/ladino/core_test.clj
+++ test/ladino/core_test.clj
@@ -1,7 +1,36 @@
 (ns ladino.core-test
-  (:use clojure.test
-        ladino.core))
+  (:require [midje.sweet :refer :all]
+            [ladino.core :refer :all]
+            [ladino.peer :as p]
+            ;; Data sources
+            [ladino.sources.stemlist :as lss]
+            [ladino.sources.endings  :as lse]
+            [ladino.parse :as parse]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+
+
+(fact "on how stuff from the .lat files ends up in the database"
+      (future-fact "we pass the lines to the sources files")
+      (import-from-sources!) => irrelevant
+      (provided
+       (lse/read-endings-file)  => [anything anything]
+       (lss/read-stemlist-file) => [anything]
+       (p/transact-one anything) => irrelevant))
+
+(fact "on how we process command-line arguments"
+      (future-fact "on multiple arguments")
+      (-main ..veritas..) => ..truth..
+      (provided
+       (initialize) => irrelevant
+       (parse/parse ..veritas..) => ..truth..))
+
+(fact "on the steps of `initialize!`"
+      (initialize!) => irrelevant
+      (provided
+       (init-db!) => anything
+       (import-from-sources!) => anything))
+
+(future-fact "on init-db!"
+   (future-fact "on how it's in components, not here")
+   (future-fact "on it's various parts, that should be broken down")
+   (future-fact "on how we don't def the database, but rather set it on an atom"))
